a7a473e9617f1fe8ae0dd55d8ae952105c1fa152
Qasim <106329169+H3x0ra@users.noreply.github.com>
2026-01-21 09:23:06 +0300

    Implement typing effect and image slideshow
    Add typing animation and certificate image rotation

--- script.js
+++ script.js
@@ -1,2 +1,25 @@
-// Future animations or interactions can go here
-console.log("Portfolio Loaded Successfully");
+new Typed(".typed", {
+  strings: [
+    "SOC Analyst",
+    "Active Directory Security",
+    "Incident Response",
+    "Ethical Hacking"
+  ],
+  typeSpeed: 60,
+  backSpeed: 40,
+  loop: true
+});
+
+const certs = [
+  "IMAGES/Linux.jpg",
+  "IMAGES/LRSA.jpg",
+  "IMAGES/LRPA.jpg",
+  "IMAGES/CyberX.jpg",
+  "IMAGES/PEH.jpg"
+];
+
+let i = 0;
+setInterval(() => {
+  document.getElementById("certImg").src = certs[i];
+  i = (i + 1) % certs.length;
+}, 4000);
